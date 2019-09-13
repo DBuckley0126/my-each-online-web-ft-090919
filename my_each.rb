3c939b25dac615311ddf1aaa7ecbdfbbed9f73e8
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-13 12:33:43 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,19 @@
+#def my_each(array)
+#  i = 0
+ #while i < array.length
+  #  yield(array[i])
+  #  i += 1
+#  end
+#array
+#end
+
 def my_each(array)
-  i = 0
- while i < array.length
-    yield(array[i])
-    i += 1
-  end
-array
+ i = 0
+ while i < array.length do
+   yield array[i]
+   i += 1
+ end
 end
+ my_each(array) do
+   puts "#{i}"
+ end
